03b1223eb9cc23dc26d8ec59569cb08f16eea0ee
Prashanth-naidu <saiprashanthnaidu68@gmail.com>
2024-06-11 22:25:12 +0530

    Update ChefJpaService.java

--- src/main/java/com/example/dinemaster/service/ChefJpaService.java
+++ src/main/java/com/example/dinemaster/service/ChefJpaService.java
@@ -1,46 +1,105 @@
-package com.example.dinemaster.controller;
+package com.example.dinemaster.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import java.util.ArrayList;
+import java.util.List;
 
-import com.example.dinemaster.model.Chef;
 import com.example.dinemaster.model.Restaurant;
-import com.example.dinemaster.service.ChefJpaService;
+import com.example.dinemaster.model.Chef;
+import com.example.dinemaster.repository.ChefRepository;
+import com.example.dinemaster.repository.ChefJpaRepository;
+import com.example.dinemaster.repository.RestaurantJpaRepository;
 
-@RestController
-public class ChefJpaService {
+@Service
+public class ChefJpaService implements ChefRepository {
 
     @Autowired
-    private ChefJpaService s;
+    private ChefJpaRepository db;
 
-    @GetMapping("/restaurants/chefs")
+    @Autowired
+    private RestaurantJpaRepository db1;
+
+    @Override
     public ArrayList<Chef> getChefs() {
-        return s.getChefs();
+        return (ArrayList<Chef>) db.findAll();
     }
 
-    @GetMapping("/restaurants/chefs/{chefId}")
-    public Chef getChefById(@PathVariable("chefId") int chefId) {
-        return s.getChefById(chefId);
+    @Override
+    public Chef getChefById(int Id) {
+        try {
+            Chef chef = db.findById(Id).get();
+            return chef;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PostMapping("/restaurants/chefs")
-    public Chef addChef(@RequestBody Chef chef) {
-        return s.addChef(chef);
+    @Override
+    public Chef addChef(Chef chef) {
+        Restaurant restaurant = chef.getRestaurant();
+        int restaurantId = restaurant.getId();
+        try {
+            restaurant = db1.findById(restaurantId).get();
+            chef.setRestaurant(restaurant);
+            db.save(chef);
+
+            return chef;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PutMapping("/restaurants/chefs/{chefId}")
-    public Chef updateChef(@PathVariable("chefId") int chefId, @RequestBody Chef chef) {
-        return s.updateChef(chefId, chef);
+    @Override
+    public Chef updateChef(int id, Chef chef) {
+        try {
+            Chef newOne = db.findById(id).get();
+            if (chef.getFirstName() != null) {
+                newOne.setFirstName(chef.getFirstName());
+            }
+            if (chef.getLastName() != null) {
+                newOne.setLastName(chef.getLastName());
+            }
+            if (chef.getExpertise() != null) {
+                newOne.setExpertise(chef.getExpertise());
+            }
+            if (chef.getExperienceYears() != 0) {
+                newOne.setExperienceYears(chef.getExperienceYears());
+            }
+            if (chef.getRestaurant() != null) {
+                int restaurantId = chef.getRestaurant().getId();
+                Restaurant newRestaurant = db1.findById(restaurantId).get();
+                newOne.setRestaurant(newRestaurant);
+            }
+
+            db.save(newOne);
+
+            return newOne;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @DeleteMapping("/restaurants/chefs/{chefId}")
-    public void deleteChef(@PathVariable int chefId) {
-        s.deleteChef(chefId);
+    @Override
+    public Chef deleteChef(int id){
+        try {
+            db.deleteById(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
     }
 
-    @GetMapping("/chefs/{chefId}/restaurant")
-    public Restaurant getChefRestaurant(@PathVariable int chefId) {
-        return s.getChefRestaurant(chefId);
+    @Override
+    public Restaurant getChefRestaurant(int id) {
+        try {
+            Chef chef = db.findById(id).get();
+            Restaurant restaurant = chef.getRestaurant();
+            return restaurant;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
